882eb0cc43f86781f21d31d1c39a52d6b23c96bf
adityabhartib <adityabharti.ab@gmail.com>
2022-11-10 14:11:49 +0530

    Added :Refactor

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -1,6 +1,10 @@
 package com.bridgelabz;
 
 public class MoodAnalyser {
+    private String message;
+    public void MoodAnalyer(String message) {
+        this.message = message;
+    }
     public String moodAnalyser(String message) {
         if (message.contains("Sad")) {
             return "SAD";
