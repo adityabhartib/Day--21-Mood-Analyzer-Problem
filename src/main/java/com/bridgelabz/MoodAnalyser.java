28de817ff6e0fe6895d8961cbc52955c15ca7c19
adityabhartib <adityabharti.ab@gmail.com>
2022-11-10 13:38:14 +0530

    Added :TC1.1 I am in sadd Mood

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -1,11 +1,11 @@
 package com.bridgelabz;
 
 public class MoodAnalyser {
-    public String moodAnalyser(String message)
-    {
-        if (message.contains("Sad"))
+    public String moodAnalyser(String message) {
+        if (message.contains("Sad")) {
             return "SAD";
-
-        return "HAPPY";
+        } else {
+            return "HAPPY";
+        }
     }
 }
