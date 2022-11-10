9e1e20a4160d2e554c7a2fa1a4b8cd0bbbef5b27
adityabhartib <adityabharti.ab@gmail.com>
2022-11-10 13:53:07 +0530

    Added :TC1.2 I am in any Mood

--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -28,7 +28,7 @@ public class MoodAnalyzerTest {
     }
     @Test
     public void moodAnalyser1(){
-        String ans1 = object.moodAnalyser1("I am in any Mood");
+        String ans1 = object.moodAnalyser("I am in any Mood");
         Assertions.assertEquals("HAPPY",ans1);;
         System.out.println(ans1);
     }
